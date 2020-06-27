76f4c7433cc8a14bacf9714b67822ca5494e0647
JMACBigMan <jemclaurin@gmail.com>
2020-06-26 22:20:51 -0400

    Updates

---
+++ precache-manifest.8413aa575ce5fbeb8fab3a6f47cb494c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b08b43049272a9e737220b92d421728",
+    "url": "/tasks/index.html"
+  },
+  {
+    "revision": "079685fefdd61a070e72",
+    "url": "/tasks/static/css/main.232a63d7.chunk.css"
+  },
+  {
+    "revision": "a6d311ab53f46d51e5d4",
+    "url": "/tasks/static/js/2.ab46d6d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tasks/static/js/2.ab46d6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079685fefdd61a070e72",
+    "url": "/tasks/static/js/main.252a5575.chunk.js"
+  },
+  {
+    "revision": "af5b32f645a59ebd24de",
+    "url": "/tasks/static/js/runtime-main.ee079799.js"
+  }
+]);
